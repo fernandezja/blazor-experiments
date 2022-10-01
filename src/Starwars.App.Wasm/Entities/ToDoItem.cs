b4ba3ab890870736c4d64ee3d471c9c2b58ad37c
fernandezja <fernandezja@gmail.com>
2022-10-01 10:50:11 -0300

    [Wasm] ToDo with actions

---
+++ src/Starwars.App.Wasm/Entities/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Starwars.App.Wasm.Entities
+{
+    public class ToDoItem
+    {
+        public int TodoItemId { get; set; }
+        public string? Titulo { get; set; }
+        public string? Contenido { get; set; }
+        public string? Responsable { get; set; }
+        public List<string> Etiquetas { get; set; } = new List<string>();
+        public DateOnly? FechaEntrega { get; set; }
+        public TimeOnly? HoraEntrega { get; set; }
+        public bool EstaCompletado { get; set; } = false;
+    }
+}
