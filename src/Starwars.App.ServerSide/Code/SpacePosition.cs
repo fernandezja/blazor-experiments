d4d2abe5b13ff452b32e536d8b66e3450fa03552
fernandezja <fernandezja@gmail.com>
2023-03-26 18:38:37 -0300

    [ServerSide] Dynamic Component

---
+++ src/Starwars.App.ServerSide/Code/SpacePosition.cs
@@ -0,0 +1,9 @@
+﻿namespace Starwars.App.ServerSide
+{
+    public class SpacePosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
